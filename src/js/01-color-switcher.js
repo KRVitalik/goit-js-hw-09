8d127eff7f2fb29c260e3e10437d01a8365b23f4
KRVitalik <karmelitavitalik@gmail.com>
2023-05-09 13:43:38 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const randomBodyColor = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let intervalId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+  if (intervalId) {
+    return
+  };
+    intervalId = setInterval(() => {
+      randomBodyColor.style.backgroundColor = `${getRandomHexColor()}`
+    }, 1000);
+};
+
+function onBtnStop() {
+  if (!intervalId) {
+    return
+  };
+  clearInterval(intervalId);
+  intervalId = false;
+}
